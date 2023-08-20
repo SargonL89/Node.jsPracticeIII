a8572019d2923542797397a329fd5c22072a262f
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-08-20 13:43:26 -0300

    HTTP Methods

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const {infoCursos} = require('./datos/cursos.js');
+
+
+//Routers
+const routerProgramacion = require('./routers/programacion.js');
+app.use('/api/cursos/programacion', routerProgramacion);
+
+const routerMatematicas = require('./routers/matematicas.js')
+app.use('/api/cursos/matematicas', routerMatematicas);
+
+
+//Routing
+app.get('/', (req, res) => {
+    res.send('Mi primer servidor con Express. Cursos');
+});
+
+app.get('/api/cursos', (req, res) => {
+    res.send(infoCursos);
+});
+
+
+
+
+
+const PUERTO = process.env.PORT || 3000
+
+app.listen(PUERTO, () => {
+    console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`)
+});
---
+++ datos/cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+    programacion: [
+        {
+            id: 1,
+            titulo: 'python basico',
+            lenguaje: 'python',
+            vistas: 45919,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo: 'python avanzado',
+            lenguaje: 'python',
+            vistas: 34357,
+            nivel: 'avanzado'
+        },
+        {
+            id: 3,
+            titulo: 'javascript',
+            lenguaje: 'javascript',
+            vistas: 234435,
+            nivel: 'intermedio'
+        }
+    ],
+    matematicas: [
+        {
+            id: 1,
+            titulo: 'calculo',
+            tema: 'calculo',
+            vistas: 23469,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo: 'aprende algebra',
+            tema: 'algebra',
+            vistas: 420451,
+            nivel: 'basico'
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos
